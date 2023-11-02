dc0c8a681498a576905e4aa606195270e74f7adf
李文芳 <120286899+JularDepick@users.noreply.github.com>
2023-11-02 17:20:06 +0800

    Add files via upload

---
+++ Yuameng/Expand/Prog/MusicPlayer/js/js/back.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var total=12;
+    var head = document.getElementById("-body");
+	var i = 0;
+    function time() {
+		i++;
+		if(i>total) {
+			i=1;
+		}
+        head.style.backgroundImage = "url(./images/bg" +i+ ".jpg)";
+    }
+    setInterval(time, 10*1000);
+}
